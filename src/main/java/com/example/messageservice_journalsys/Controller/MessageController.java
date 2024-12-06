2d2e141780399d4a2368c4b02e59e46cdbc42ff5
Labb3A <filipkleimert@gmail.com>
2024-12-06 21:46:55 +0100

    Fixed CORS for kubernetes

--- src/main/java/com/example/messageservice_journalsys/Controller/MessageController.java
+++ src/main/java/com/example/messageservice_journalsys/Controller/MessageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://kubernetes.docker.internal:30000")
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
